3e52f98dac1d7166b463f4daf1517b695a3a3277
Ben Chatelain <ben@octop.ad>
2016-09-30 16:22:39 -0600

    Add mobot4 alias

--- .dotfiles/shell/ssh.zsh
+++ .dotfiles/shell/ssh.zsh
@@ -13,6 +13,7 @@ alias buildsrvpro='ssh admin@buildsrvpro.co.kp.org'
 alias buildsrvpro2='ssh admin@buildsrvpro.ptc.ca.kp.org'
 alias mobot2='ssh admin@RMGVIMACSVR002.co.kp.org'
 alias mobot3='ssh admin@RMGVIMACSVR003.co.kp.org'
+alias mobot4='ssh admin@RMGVIMACSVR004.co.kp.org'
 alias realmdev='ssh f978034@cskpcloudxn0790.cloud.kp.org'
 
 # SSH Utility Aliases
