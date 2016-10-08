d8cca0358b22ec02e48bd57f76884f386c49ad95
Ben Chatelain <ben@octop.ad>
2016-10-07 19:47:14 -0600

    Clean up and document SSH aliases

--- .dotfiles/shell/ssh.zsh
+++ .dotfiles/shell/ssh.zsh
@@ -16,41 +16,68 @@ alias mobot3='ssh admin@RMGVIMACSVR003.co.kp.org'
 alias mobot4='ssh admin@RMGVIMACSVR004.co.kp.org'
 alias realmdev='ssh f978034@cskpcloudxn0790.cloud.kp.org'
 
-# SSH Utility Aliases
+
+#-------------------------------------------------------------------------------
+#
+# SSh
+#
+#-------------------------------------------------------------------------------
+
+# Aliases
 alias sshshowpub='cat ~/.ssh/id_rsa.pub'
 alias sshcopypub='pbcopy < ~/.ssh/id_rsa.pub'
 
+# Functions
+
+# sshkeyfingerprint
+#
+# Prints out information about a key in the following format:
+# > sshkeyfingerprint [/Users/<USER>/.ssh/id_rsa.pub] 2048 SHA256:<PUBLIC_KEY> <COMMENT> (RSA)
+#
+# - arg $0 - Optional path to key file, defaults to ~/.ssh/id_rsa.pub
 function sshkeyfingerprint {
   file=~/.ssh/id_rsa.pub;
   if (($+1)); then
     file="$1"
-  else
-    file=~/.ssh/id_rsa.pub
   fi
+
   echo -n "sshkeyfingerprint [${file}] "
   ssh-keygen -lf "${file}";
 }
-alias sshkeyfingerprint=sshkeyfingerprint
 
-alias sshnewkey=sshnewkey
+# sshnewkey
+#
+# Generates a new for use with outgoing SSH connections.
+#
+# - arg $0 - Optional key comment (typically user@hostname).
 function sshnewkey {
+  comment="${USER}@${HOST}"
   if (($+1)); then
-    ssh-keygen -t rsa -C "$1"
-  else
-    ssh-keygen -t rsa -C "${USER}@${HOST}"
+    comment="$1"
   fi
+
+  ssh-keygen -t rsa -C "${comment}"
 }
 
+# sshupload
+#
+# Uploads a public key to GitHub. Asks for authentication token before using curl to post the key.
+#
+# - arg $0 - Optional path to key file, defaults to ~/.ssh/id_rsa.pub
 function sshupload {
+  keyfile="~/.ssh/id_rsa.pub"
+  if (($+1)); then
+    keyfile="$1"
+  fi
+
   # Upload default SSH key to GitHub
-  echo "Uploading SSH public key to GitHub"
+  echo "Uploading SSH public key to GitHub [${keyfile}]"
   echo -n "GitHub OTP code: "
   read otpcode
   curl -X POST \
     --user "phatblat" \
     --header "X-GitHub-OTP: ${otpcode}" \
-    --data "{\"title\":\"${USER}@${HOST}_$(date +%Y%m%d%H%M%S)\",\"key\":\"$(cat ~/.ssh/id_rsa.pub)\"}" \
+    --data "{\"title\":\"${USER}@${HOST}_$(date +%Y%m%d%H%M%S)\",\"key\":\"$(cat $keyfile)\"}" \
     --verbose \
-    https://api.github.com/user/keys
+    "https://api.github.com/user/keys"
 }
-alias sshupload=sshupload
