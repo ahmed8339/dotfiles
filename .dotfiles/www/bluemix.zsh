9b9c6cc2d0fb8de6423cef9d7f7cdbce09d34c42
Ben Chatelain <ben@octop.ad>
2017-05-03 17:26:19 -0600

    Wrap cf commands in bmx function

--- .dotfiles/www/bluemix.zsh
+++ .dotfiles/www/bluemix.zsh
@@ -7,5 +7,10 @@
 
 lj info 'www/bluemix.zsh'
 
-cf api https://api.ng.bluemix.net
-cf login -u ben.d.chatelain@kp.org -o "Kaiser Permanente Bluemix PoC" -s "CSDT Mobile Team"
+function bmx {
+  cf api https://api.ng.bluemix.net
+  cf login \
+    -u ben.d.chatelain@kp.org \
+    -o "Kaiser Permanente Bluemix PoC" \
+    -s "CSDT Mobile Team"
+}
