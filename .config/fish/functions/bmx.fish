da0b7b2df2b05d98426ef8e11979b867077fda3d
Ben Chatelain <ben@octop.ad>
2017-05-08 16:53:12 -0600

    Add all ported zsh functions

---
+++ .config/fish/functions/bmx.fish
@@ -0,0 +1,8 @@
+# 
+function bmx
+      cf api https://api.ng.bluemix.net
+  cf login \
+    -u ben.d.chatelain@kp.org \
+    -o "Kaiser Permanente Bluemix PoC" \
+    -s "CSDT Mobile Team" $argv
+end
