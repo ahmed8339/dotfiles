58ab5c1fb58719152c46079373aca56ed7d3db01
Ben Chatelain <ben@octop.ad>
2017-05-08 22:42:21 -0600

    Formatting

--- .config/fish/functions/bmx.fish
+++ .config/fish/functions/bmx.fish
@@ -1,8 +1,9 @@
-# 
+# Logs into Bluemix.
 function bmx
-      cf api https://api.ng.bluemix.net
-  cf login \
+    cf api https://api.ng.bluemix.net
+    cf login \
     -u ben.d.chatelain@kp.org \
     -o "Kaiser Permanente Bluemix PoC" \
-    -s "CSDT Mobile Team" $argv
+    -s "CSDT Mobile Team" \
+    $argv
 end
